8e358090aa3fc58426eae70d06e361683105fdd2
maus912 <hun991122@gmail.com>
2019-06-04 12:08:26 +0900

    190604 수정 사항

--- boot-thymeleaf/src/main/java/idu/cs/controller/UserController.java
+++ boot-thymeleaf/src/main/java/idu/cs/controller/UserController.java
@@ -19,82 +19,90 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 import idu.cs.domain.User;
+import idu.cs.entity.UserEntity;
 import idu.cs.exception.ResourceNotFoundException;
 import idu.cs.repository.UserRepository;
+import idu.cs.service.UserService;
 
 @Controller
 // anotation : 컴파일러에게 설정 내용이나 상태를 알려주는 목적, 적용범위가 클래스 내부로 한정
-public class HomeController {
-	@Autowired UserRepository userRepo; // Dependency Injection
-	
+public class UserController {
+	//@Autowired UserRepository userRepo; // Dependency Injection
+	@Autowired UserService userService;
 	@GetMapping("/test")
 	public String home(Model model) {
 		model.addAttribute("test","인덕컴소");
 		model.addAttribute("egy","유응구");
 		return "index";
 	}
+	
 	@GetMapping("/users")
 	public String getAllUser(Model model) {
-		model.addAttribute("users",userRepo.findAll());
+		//model.addAttribute("users",userRepo.findAll());
+		model.addAttribute("users",userService.getUsers());
 		return "userlist";
 	}
-	
+	/*
 	@GetMapping("/users/byname")
 	public String getUsersByName(@Param(value = "name") String name, Model model) {
-		List<User> users = userRepo.findByName(name);
+		List<UserEntity> users = userRepo.findByName(name);
 		model.addAttribute("users", users);
 		return "userlist";
 	}
 	
 	@GetMapping("/users/nameasc")
 	public String getUsersByNameAsc(@Param(value = "name") String name, Model model) {
-		List<User> users = userRepo.findByNameOrderByIdAsc(name);
+		List<UserEntity> users = userRepo.findByNameOrderByIdAsc(name);
 		model.addAttribute("users", users);
 		return "userlist";
 	}
-	
+	*/
 	@GetMapping("/users/{id}")
 	public String getUserById(@PathVariable(value = "id") Long userId, Model model)
 	throws ResourceNotFoundException { 
 		//User user = userRepo.findById(userId).get();
-		User user = userRepo.findById(userId).orElseThrow(() -> 
-			new ResourceNotFoundException("not found " + userId ));
+		User user = userService.getUser(userId);
+				//userRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("not found " + userId ));
 		model.addAttribute("user", user);
 		
 		return "info";
 	}
 	
+	
+	/*
 	@PutMapping("/users/{id}")	
 	//@RequestMapping(value="/users/{id}" method=RequestMethod.UPDATE)
 	public String updateUserById(@PathVariable(value = "id") Long userId,
-			@Valid  User userDetails, Model model)
+			@Valid  UserEntity userDetails, Model model)
 	//userDetails 폼을 통해 전송된 객체, user는 id로 jpa를 통해서 가져온 객체
 	throws ResourceNotFoundException { 
 		//User user = userRepo.findById(userId).get();
-		User user = userRepo.findById(userId)//userDetails.getId())
+		UserEntity user = userRepo.findById(userId)//userDetails.getId())
 				.orElseThrow(() -> 
 			new ResourceNotFoundException("not found " + userId ));
 		user.setName(userDetails.getName());
 		user.setCompany(userDetails.getCompany());
-		User userUpdate = userRepo.save(user); //객체 삭제 -> jpa : record 삭제로 적용
+		UserEntity userUpdate = userRepo.save(user); //객체 삭제 -> jpa : record 삭제로 적용
 		
 		//model.addAttribute("user", userUpdate);
 		return "redirect:/users"; // 업데이트가 성공하면 users 자원을 get방식으로 model에 user어트리뷰트를 전달하면서 redirection
 		//return ResponseEntity.ok(userUpdate);
 	}
+	*/
 	
+	/*
 	@DeleteMapping("/users/{id}")	
 	//@RequestMapping(value="/users/{id}" method=RequestMethod.DELETE)
 	public String deleteUserById(@PathVariable(value = "id") Long userId, Model model)
 	throws ResourceNotFoundException { 
 		//User user = userRepo.findById(userId).get();
-		User user = userRepo.findById(userId).orElseThrow(() -> 
+		UserEntity user = userRepo.findById(userId).orElseThrow(() -> 
 			new ResourceNotFoundException("not found " + userId ));
 		userRepo.delete(user); //객체 삭제 -> jpa : record 삭제로 적용
 		model.addAttribute("name", user.getName());
 		return "disjoin";
 	}
-	
+	*/
 	@GetMapping("/")
 	public String loadWelcome(Model model) {
 		return "index";
@@ -106,9 +114,9 @@ public class HomeController {
 	}
 	// 실제 로그인 처리, user : 입력한 내용에 대한 객체, sessionUser : repository로부터 가져온 내용의 객체
 	@PostMapping("/login")
-	public String loginUser(@Valid User user, HttpSession session) {
+	public String loginUser(@Valid UserEntity user, HttpSession session) {
 		System.out.println("login process : ");
-		User sessionUser = userRepo.findByUserId(user.getUserId());
+		User sessionUser = userService.getUserByUserId(user.getUserId());
 		if(sessionUser == null) {
 			System.out.println("id error : ");
 			return "redirect:/login-form";
@@ -136,13 +144,14 @@ public class HomeController {
 	
 	@PostMapping("/users")
 	public String createUser(@Valid User user, Model model) {
-		userRepo.save(user);
-		model.addAttribute("users",userRepo.findAll());
-		return "redirect:/";
+		userService.saveUser(user);
+		//model.addAttribute("users",userService.getUsers());
+		return "redirect:/users";
 	}
-	
+	/*
 	@GetMapping("/disjoin")
 	public String disjoinForm(Model model) {
 		return "disjoin";
 	}
+	*/
 }
